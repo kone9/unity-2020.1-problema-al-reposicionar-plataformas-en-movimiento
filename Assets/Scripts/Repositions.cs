50c2863de7cda606d8946db0eb366d743005b6c2
unknown <arielgimenez_95@hotmail.com>
2020-09-13 20:52:15 -0300

    las plataformas se reposicionan mal

---
+++ Assets/Scripts/Repositions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repositions : MonoBehaviour
+{
+    
+    public float velocidadJuego = 1;
+    public Transform emphtyReposicionar;
+    
+    private void Update()
+    {   
+        transform.Translate(Time.deltaTime * -velocidadJuego, 0, 0);
+
+        if(this.transform.position.x <= -10)
+        {
+            float posicionEmphty =  emphtyReposicionar.position.x + 5;
+            print("el suelo tendría que reposicionarse");
+            //this.transform.Translate(new Vector3(this.transform.position.x,-30,this.transform.position.z),Space.World);
+          
+            this.transform.position = new Vector3(
+                posicionEmphty,
+                this.transform.position.y,
+                 this.transform.position.z
+                );
+            
+            print("la posicion del empty es: " + posicionEmphty);
+            print("la posicion de la plataforma es : " + this.transform.position.z);
+            print("----------------------------------------------------------------");     
+        }
+    }
+}
