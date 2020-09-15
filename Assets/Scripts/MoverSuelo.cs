0121a9690efe20b31b92ba29838f2288e22aafc6
unknown <arielgimenez_95@hotmail.com>
2020-09-15 08:37:13 -0300

    funciona reposicionar usando 2 script separados

---
+++ Assets/Scripts/MoverSuelo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverSuelo : MonoBehaviour
+{
+    public float velocidadJuego = 1;
+    
+    private void FixedUpdate()
+    {
+        transform.Translate(Time.deltaTime * -velocidadJuego, 0, 0);
+        
+    }
+}
---
+++ Assets/Scripts/Reposicionar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposicionar : MonoBehaviour
+{
+    public Transform reposicionar;
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.x <= -15)
+        {
+            float Acomodar =  reposicionar.position.x;
+            print("el suelo tendría que reposicionarse");
+            //this.transform.Translate(new Vector3(this.transform.position.x,-30,this.transform.position.z),Space.World);
+            
+            this.transform.position = new Vector3(
+                Acomodar,//El objeto se reposiciona en donde esta el empty
+                this.transform.position.y,
+                this.transform.position.z
+                );
+            
+            print("la posicion del empty es: " + Acomodar);
+            print("la posicion de la plataforma es : " + this.transform.position.x);
+            print("----------------------------------------------------------------");     
+        }
+    }
+    
+}
